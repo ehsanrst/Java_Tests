df1a4aeb238c852044eeea6b35d5ae51a9371ba5
Ehsan Roostai <ehsan.roostai@gmail.com>
2018-05-09 21:12:41 +0430

    Add java project tests about JFrame

--- Project1/src/Class3.java
+++ Project1/src/Class3.java
@@ -2,6 +2,10 @@
 public class Class3 extends Class1 {
 	// Override print method over parent print method
 	public static void print() {
+		/*
+		 * ATTENTION: IF this override method be non-static then we can use
+		 * @Override TAG before this method here!!!
+		 */
 		System.out.println("print in Class3");
 	}
 
